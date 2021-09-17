cad95e9f0f4c743838077b926224c325fcd8d715
gabriel carrasco <gabri136@hotmail.it>
2021-09-17 16:02:14 +0200

    complete comunication button/dialog

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ function openModal(idModal, el) {
     // const event = new Event('open', {bubbles: true, capture: false, composed: true});
     // el.dispatchEvent(event);
     // console.log(idModal, event, event.target.closest('ul'))
-    document.querySelector(`dialog#${idModal}`).setAttribute('open', true)
+    let modal = document.querySelector(`[data-id="${idModal}"]`)
+    modal.setAttribute('data-open', 'open')
 }
 
 // document.addEventListener('open', () => {
 //     console.log("sto ricevendo open dal document")
 // })
+
